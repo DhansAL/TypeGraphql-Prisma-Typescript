080c521c0fb5efc92d0883017a94c467544ae6c1
Dhans <sgdhans@gmail.com>
2022-09-04 23:57:29 +0530

    removing generated files and migrations

--- prisma/generated/type-graphql/enums/index.ts
+++
@@ -1,4 +0,0 @@
-export { QueryMode } from "./QueryMode";
-export { SortOrder } from "./SortOrder";
-export { TransactionIsolationLevel } from "./TransactionIsolationLevel";
-export { UserScalarFieldEnum } from "./UserScalarFieldEnum";
